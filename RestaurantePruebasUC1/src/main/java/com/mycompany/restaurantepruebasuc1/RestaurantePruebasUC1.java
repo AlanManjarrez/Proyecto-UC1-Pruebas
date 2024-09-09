02282fd3d66c62756c9548b01ff639d6ddc06b6a
AlanManjarrez <160210523+AlanManjarrez@users.noreply.github.com>
2024-09-08 20:40:14 -0700

    Capa de Presentación
    Puras pantallas

---
+++ RestaurantePruebasUC1/src/main/java/com/mycompany/restaurantepruebasuc1/RestaurantePruebasUC1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.restaurantepruebasuc1;
+
+/**
+ *
+ * @author uirtis
+ */
+public class RestaurantePruebasUC1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
